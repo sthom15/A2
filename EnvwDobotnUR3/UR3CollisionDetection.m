5d57a58d95c9ebb704b9a223cc7106a7bd8f3511
jan_s <jan_s@LAPTOP-NS5T33EL>
2024-11-10 22:22:49 +1100

    with gui

--- EnvwDobotnUR3/UR3CollisionDetection.m
+++ EnvwDobotnUR3/UR3CollisionDetection.m
@@ -149,4 +149,179 @@ function Ass2EnvironmentWithCollisionDetection
             collision = true;
         end
     end
+
+
+ %% Gui Code
+       qc = q0_ur3; % Joint configuration
+% Initialize the robot models and joint configurations
+qc_ur3 = q0_ur3; % Initial joint configuration for UR3
+qc_dobot = q0_dobot; % Initial joint configuration for Dobot
+
+% Set up the end-effector initial transformation and position for both robots
+end_effector_pos_ur3 = transl(r_ur3.model.fkine(qc_ur3)); % UR3
+end_effector_pos_dobot = transl(r_dobot.model.fkine(qc_dobot)); % Dobot
+% Display debugging info
+disp('Starting GUI and initializing end-effector positions for UR3 and Dobot...');
+
+   % GUI Controls for Emergency Stop and Resume
+    uicontrol('Style', 'pushbutton', 'Position', [600, 100, 100, 40], ...
+              'String', 'E-Stop', 'Callback', @emergency_stop);
+    uicontrol('Style', 'pushbutton', 'Position', [600, 50, 100, 40], ...
+              'String', 'Resume', 'Callback', @resume_operation);
+% Cartesian position inputs for UR3
+uicontrol('Style', 'text', 'Position', [50, 300, 100, 20], 'String', 'UR3 X Position');
+x_input_ur3 = uicontrol('Style', 'edit', 'Position', [150, 300, 100, 20], ...
+                        'String', num2str(end_effector_pos_ur3(1)), 'Callback', @(src, ~) update_cartesian_position_ur3(src, 1));
+uicontrol('Style', 'text', 'Position', [50, 270, 100, 20], 'String', 'UR3 Y Position');
+y_input_ur3 = uicontrol('Style', 'edit', 'Position', [150, 270, 100, 20], ...
+                        'String', num2str(end_effector_pos_ur3(2)), 'Callback', @(src, ~) update_cartesian_position_ur3(src, 2));
+uicontrol('Style', 'text', 'Position', [50, 240, 100, 20], 'String', 'UR3 Z Position');
+z_input_ur3 = uicontrol('Style', 'edit', 'Position', [150, 240, 100, 20], ...
+                        'String', num2str(end_effector_pos_ur3(3)), 'Callback', @(src, ~) update_cartesian_position_ur3(src, 3));
+
+% Cartesian position inputs for Dobot
+uicontrol('Style', 'text', 'Position', [300, 300, 100, 20], 'String', 'Dobot X Position');
+x_input_dobot = uicontrol('Style', 'edit', 'Position', [400, 300, 100, 20], ...
+                          'String', num2str(end_effector_pos_dobot(1)), 'Callback', @(src, ~) update_cartesian_position_dobot(src, 1));
+uicontrol('Style', 'text', 'Position', [300, 270, 100, 20], 'String', 'Dobot Y Position');
+y_input_dobot = uicontrol('Style', 'edit', 'Position', [400, 270, 100, 20], ...
+                          'String', num2str(end_effector_pos_dobot(2)), 'Callback', @(src, ~) update_cartesian_position_dobot(src, 2));
+uicontrol('Style', 'text', 'Position', [300, 240, 100, 20], 'String', 'Dobot Z Position');
+z_input_dobot = uicontrol('Style', 'edit', 'Position', [400, 240, 100, 20], ...
+                          'String', num2str(end_effector_pos_dobot(3)), 'Callback', @(src, ~) update_cartesian_position_dobot(src, 3));
+
+% Joint angle control for UR3
+joint_index_ur3 = 1; % Default joint index for UR3
+
+uicontrol('Style', 'text', 'Position', [50, 200, 100, 20], 'String', 'UR3 Select Joint');
+joint_dropdown_ur3 = uicontrol('Style', 'popupmenu', 'Position', [150, 200, 100, 20], ...
+                               'String', {'Joint 1', 'Joint 2', 'Joint 3', 'Joint 4', 'Joint 5', 'Joint 6'}, ...
+                               'Callback', @update_joint_index_ur3);
+uicontrol('Style', 'text', 'Position', [50, 170, 100, 20], 'String', 'UR3 Joint Angle');
+joint_input_ur3 = uicontrol('Style', 'edit', 'Position', [150, 170, 100, 20], ...
+                            'String', num2str(qc_ur3(joint_index_ur3)), 'Callback', @(src, ~) update_joint_angle_ur3(src));
+angle_display_ur3 = uicontrol('Style', 'text', 'Position', [260, 170, 100, 20], 'String', 'Angle: 0');
+
+% Joint angle control for Dobot
+joint_index_dobot = 1; % Default joint index for Dobot
+
+uicontrol('Style', 'text', 'Position', [300, 200, 100, 20], 'String', 'Dobot Select Joint');
+joint_dropdown_dobot = uicontrol('Style', 'popupmenu', 'Position', [400, 200, 100, 20], ...
+                                 'String', {'Joint 1', 'Joint 2', 'Joint 3', 'Joint 4', 'Joint 5', 'Joint 6', 'Joint 7', 'Joint 8'}, ...
+                                 'Callback', @update_joint_index_dobot);
+uicontrol('Style', 'text', 'Position', [300, 170, 100, 20], 'String', 'Dobot Joint Angle');
+joint_input_dobot = uicontrol('Style', 'edit', 'Position', [400, 170, 100, 20], ...
+                              'String', num2str(qc_dobot(joint_index_dobot)), 'Callback', @(src, ~) update_joint_angle_dobot(src));
+angle_display_dobot = uicontrol('Style', 'text', 'Position', [510, 170, 100, 20], 'String', 'Angle: 0');
+
+%% Callback Functions for UR3
+function update_cartesian_position_ur3(src, axis)
+    new_value = str2double(src.String);
+    if isnan(new_value)
+        disp('Please enter a valid number for UR3.');
+        return;
+    end
+    end_effector_pos_ur3(axis) = new_value;
+
+    target_tform = transl(end_effector_pos_ur3);
+    new_qc = r_ur3.model.ikcon(target_tform, qc_ur3);
+
+    if isempty(new_qc)
+        disp('UR3: No valid joint configuration found for the target position.');
+        return;
+    end
+
+    steps = 50;
+    qMatrix = jtraj(qc_ur3, new_qc, steps);
+
+    for i = 1:steps
+        r_ur3.model.animate(qMatrix(i, :));
+        pause(0.05);
+    end
+    qc_ur3 = new_qc;
+    disp('UR3 moved to new position.');
 end
+
+function update_joint_index_ur3(src, ~)
+    joint_index_ur3 = src.Value;
+    joint_input_ur3.String = num2str(qc_ur3(joint_index_ur3));
+end
+
+function update_joint_angle_ur3(src)
+    joint_angle = str2double(src.String);
+    qc_ur3(joint_index_ur3) = joint_angle;
+    r_ur3.model.animate(qc_ur3);
+end
+
+%% Callback Functions for Dobot
+function update_cartesian_position_dobot(src, axis)
+    new_value = str2double(src.String);
+    if isnan(new_value)
+        disp('Please enter a valid number for Dobot.');
+        return;
+    end
+    end_effector_pos_dobot(axis) = new_value;
+
+    target_tform = transl(end_effector_pos_dobot);
+    new_qc = r_dobot.model.ikcon(target_tform, qc_dobot);
+
+    if isempty(new_qc)
+        disp('Dobot: No valid joint configuration found for the target position.');
+        return;
+    end
+
+    steps = 50;
+    qMatrix = jtraj(qc_dobot, new_qc, steps);
+
+    for i = 1:steps
+        r_dobot.model.animate(qMatrix(i, :));
+        pause(0.05);
+    end
+    qc_dobot = new_qc;
+    disp('Dobot moved to new position.');
+end
+
+function update_joint_index_dobot(src, ~)
+    joint_index_dobot = src.Value;
+    joint_input_dobot.String = num2str(qc_dobot(joint_index_dobot));
+end
+
+function update_joint_angle_dobot(src)
+    joint_angle = str2double(src.String);
+    qc_dobot(joint_index_dobot) = joint_angle;
+    r_dobot.model.animate(qc_dobot);
+end
+
+           function emergency_stop(~, ~)
+        isPaused = true;  % Set the pause flag
+        if isStopped == false; % Track that E-Stop was triggered
+        disp('Emergency Stop activated.');
+        isStopped = true;
+        else
+                  isStopped = false;
+          disp('Emergency Stop Deactivated.');
+
+        end
+
+    end
+
+    % Callback for Resume button
+    function resume_operation(~, ~)
+        if isStopped == false;
+            isPaused = false; % Clear the pause flag
+            disp('Resuming operation...');
+    move_robot();
+
+        end
+        if section3run == true;
+            section1run = false
+            section2run = false
+            section3run = false
+        end
+             if section3rund == true;
+            section1rund = false
+            section2rund = false
+            section3rund = false
+        end
+    end
+  end
